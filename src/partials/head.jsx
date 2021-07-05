1f59a686e86127dbcd3f2ebb7eaa1de7e1d6d835
Manav Garg <58691392+ManavvGarg@users.noreply.github.com>
2021-07-06 02:16:13 +0530

    Icon

--- src/partials/head.jsx
+++ src/partials/head.jsx
@@ -27,6 +27,7 @@ const HeadData = (props) => {
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
 
+        <link rel="icon" type="image/x-icon" href="../../img/favicon.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
